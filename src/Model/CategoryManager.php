28381089c4c57a2785697ed93e4db056ad6f0b58
Thurzas <mathieu.miot@gmail.com>
2024-07-03 16:23:13 +0200

    adding index template

--- src/Model/CategoryManager.php
+++ src/Model/CategoryManager.php
@@ -13,7 +13,8 @@ class CategoryManager extends AbstractManager
      */
     public function insert(array $item): int
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . "(`name` ,`description`, `created_at`) VALUES (:name, :description, :created_at)");
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . "(`name` ,`description`, `created_at`)
+            VALUES (:name, :description, :created_at)");
         $statement->bindValue('name', $item['name'], PDO::PARAM_STR);
         $statement->bindValue('description', $item['description'], PDO::PARAM_STR);
         $statement->bindValue('created_at', $item['created_at'], PDO::PARAM_STR);
@@ -26,7 +27,8 @@ class CategoryManager extends AbstractManager
      */
     public function update(array $item): bool
     {
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name` = :name, `description`  = :description WHERE id=:id");
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE .
+            " SET `name` = :name, `description`  = :description WHERE id=:id");
         $statement->bindValue('id', $item['id'], PDO::PARAM_INT);
         $statement->bindValue('name', $item['name'], PDO::PARAM_STR);
         $statement->bindValue('description', $item['description'], PDO::PARAM_STR);
--- src/routes.php
+++ src/routes.php
@@ -13,4 +13,9 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
+    'categories' => ['CategoryController', 'index',],
+    'categories/edit' => ['CategoryController', 'edit', ['id']],
+    'categories/show' => ['CategoryController', 'show', ['id']],
+    'categories/add' => ['CategoryController', 'add',],
+    'categories/delete' => ['CategoryController', 'delete',],
 ];
