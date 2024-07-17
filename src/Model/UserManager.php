a0aec239a26325439cfc3b1da8ccdcf4664eca7c
chaimette <chaima_elkadiri@hotmail.fr>
2024-07-17 16:05:37 +0200

    started working on edit function

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -51,7 +51,8 @@ class UserManager extends AbstractManager
 
     public function getUserById($id)
     {
-        $statement = $this->pdo->prepare("SELECT name, lastname, password, profile_picture, mail, created_at FROM user WHERE id = :id");
+        $statement = $this->pdo->prepare("SELECT name, lastname, password, 
+        profile_picture, mail, created_at FROM user WHERE id = :id");
         $statement->bindValue('id', $id, PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetch();
